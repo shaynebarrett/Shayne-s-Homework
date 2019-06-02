b822b67aacba9f64c16da99aec5eaba1a83e2c50
shaynebarret <shayne.barrett@wsu.edu>
2019-06-02 15:52:04 -0700

    Homework 6

---
+++ Homework6/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Homework6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CardForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CardForm(Models.BirthdayCardInfo birthdayCardInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Sent", birthdayCardInfo);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ Homework6/Models/BirthdayCardInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Homework6.Models
+{
+    public class BirthdayCardInfo
+    {
+        
+        [Required(ErrorMessage = "Please enter To:")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Please enter From:")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Please enter a message")]
+        public string Message { get; set; }
+    }
+}
